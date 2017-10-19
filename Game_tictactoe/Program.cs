ba01c59d5b0241b94c86607852ab2c2479946144
Anonta <SH-anonta@users.noreply.github.com>
2017-10-19 22:31:28 +0600

    Add project files.

---
+++ Game_tictactoe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Game_tictactoe
+{
+    class Launcher
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
